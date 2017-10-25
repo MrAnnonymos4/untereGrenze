d2c8c829596f9c46668246b7c7b5914d4a97cf01
MrAnnonymos4 <31760067+MrAnnonymos4@users.noreply.github.com>
2017-10-25 17:49:56 +0200

    Update invitation.php

--- model/invitation.php
+++ model/invitation.php
@@ -1,22 +1,23 @@
 <?php
+//Returns invitation taskID
     function taskIdOfInivitationWithId($id) {
         include_once("../database/databaseConnection.php");
         return getColumnOfRowWithIdInTable("taskId", $id, "invitation");
     }
-
+//Returns invitation name
     function taskNameOfInivitationWithId($id) {
         include_once("../database/databaseConnection.php");
         $theTaskId = taskIdOfInivitationWithId($id);
         return getColumnOfRowWithIdInTable("name", $theTaskId, "task");
     }
-
+//Returns invitation creator name
     function creatorNameOfInvitationWithId($id) {
         include_once("../database/databaseConnection.php");
         include_once("task.php");
         $theTaskId = taskIdOfInivitationWithId($id);
         return creatorNameOfTaskWithId($theTaskId);
     }
-
+//Returns invitation status by description
     function statusNameForInvitationWithId($id) {
         include_once("../database/databaseConnection.php");
         $theStatusArray = array(
@@ -26,17 +27,17 @@
         );
         return $theStatusArray[statusNumberForInvitationWithId($id)];
     }
-
+//Returns invitation status by number
     function statusNumberForInvitationWithId($id) {
         include_once("../database/databaseConnection.php");
         return getColumnOfRowWithIdInTable("status", $id, "invitation");
     }
-
+//Returns vote for invitation
     function voteForInvitationWithId($id) {
         include_once("../database/databaseConnection.php");
         return getColumnOfRowWithIdInTable("vote", $id, "invitation");
     }
-
+//Create new Invitation
     function createInvitation($taskId, $userId) {
         include_once("../database/databaseConnection.php");
         $sql = "INSERT INTO invitation (taskId, playerId) VALUES ($taskId, $userId)";
