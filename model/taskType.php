7b9180be71afbdf213a2a67ae76c42e603b8ce31
MrAnnonymos4 <31760067+MrAnnonymos4@users.noreply.github.com>
2017-10-25 18:01:34 +0200

    Update taskType.php

--- model/taskType.php
+++ model/taskType.php
@@ -1,4 +1,5 @@
 <?php
+//Returns tasktype name
     function nameOfTaskTypeWithId($typeId) {
         include_once("../database/databaseConnection.php");
         return getColumnOfRowWithIdInTable("name", $typeId, "taskType");
